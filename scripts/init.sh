9e54310e091e8fc83fb377f566dc0e12a0d1bab7
Alphacode18 <skharban@purdue.edu>
2021-11-23 02:41:40 -0500

    refactor: implements complete pipe from provisioning VM, executing benchmarks, processing the results and outputting graphs

--- scripts/init.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-vagrant up
-
-for benchmark in "110.dynamic-html 501.graph-pagerank 502.graph-mst 503.graph-bfs"
-do
-    python3 ../python/processor.py $benchmark
-    python3 ../python/grapher.py $benchmark
-done
