e23ed1ac3dbc4c22f1977c7cdd45fc3a1148598b
Alphacode18 <skharban@purdue.edu>
2021-11-25 11:16:58 -0500

    refactor: removes echo statement from init scipt

--- scripts/init.sh
+++ scripts/init.sh
@@ -4,7 +4,6 @@ vagrant up
 
 for benchmark in 110.dynamic-html 501.graph-pagerank 502.graph-mst 503.graph-bfs
 do
-    echo $benchmark
     python3 ../python/processor.py $benchmark
     python3 ../python/grapher.py $benchmark
 done
