45b7f60351bd3e14d9c22e4a4bf7aebb04dfa2cd
Alphacode18 <skharban@purdue.edu>
2021-11-30 23:31:03 -0500

    feat: adds thumbnailer benchmark

---
+++ benchmarks/210.thumbnailer/init.py
@@ -0,0 +1,24 @@
+import os
+from storage import storage
+from input import generate_input
+from function import handler
+
+store = storage()
+client = store.client
+ROOT_DIR = os.path.abspath("../..")
+
+client.make_bucket("210.thumbnailer-in")
+client.make_bucket("210.thumbnailer-out")
+
+def cleanup(bucket, object_name):
+  client.remove_object(bucket, object_name)
+
+for iteration in range(0, 1000):
+  input_conf = generate_input(data_dir=os.path.join(ROOT_DIR, "benchmarks", "data", "210.thumbnailer"),
+                              input_buckets=["210.thumbnailer-in"],
+                              output_buckets=["210.thumbnailer-out"],
+                              upload_func=store.upload)
+  input_conf['object']['store'] = store
+  handler(iteration, input_conf)
+  # cleanup("210.thumbnailer-in", input_conf['object']['key'])
+  # cleanup("210.thumbnailer-out", input_conf['object']['key'])
